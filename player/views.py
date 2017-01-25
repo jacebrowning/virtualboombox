8064b495afab93e3526fe8f62e8661ce3b6172ba
Jace Browning <jacebrowning@gmail.com>
2017-01-25 18:45:16 -0500

    Connect Song model to index template

--- player/views.py
+++ player/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Song
+
 
 def index(request):
-    return render(request, 'index.html', {})
+    songs = Song.objects.order_by('date')
+    return render(request, 'index.html', {'songs': songs})
