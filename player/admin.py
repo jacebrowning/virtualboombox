6a5441a30d9da98f38b4dc95d66f55f362d36576
Jace Browning <jacebrowning@gmail.com>
2017-01-27 00:52:36 -0500

    Set admin site title

--- player/admin.py
+++ player/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Song
 
+
+admin.site.site_title = "Virtual BoomBox Admin"
+admin.site.site_header = "Virtual BoomBox"
+
 admin.site.register(Song)
