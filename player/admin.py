a32f21361571332498c84795364cd53cff1fa76f
Jace Browning <jacebrowning@gmail.com>
2017-01-27 21:52:34 -0500

    Add Account to the admin

--- player/admin.py
+++ player/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Song
+from .models import Account, Song
 
 
 admin.site.site_title = "Virtual Boombox Admin"
 admin.site.site_header = "Virtual Boombox"
 
+admin.site.register(Account)
 admin.site.register(Song)
