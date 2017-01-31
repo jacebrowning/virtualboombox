cfaf09d041b13e942ba8f0dc9f16a8df99db2a26
Jace Browning <jacebrowning@gmail.com>
2017-01-31 18:05:06 -0500

    Suppress warnings in dotenv

--- manage.py
+++ manage.py
@@ -2,13 +2,18 @@
 
 import os
 import sys
+import warnings
 
 from django.core.management import execute_from_command_line
 import dotenv
 
 
 if __name__ == '__main__':
-    dotenv.read_dotenv()
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        dotenv.read_dotenv()
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                           'virtualboombox.settings.dev')
+
     execute_from_command_line(sys.argv)
