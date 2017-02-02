446760bacf7dfaf7d3cd26e4ce7cbc2ad2fc9f67
Jace Browning <jacebrowning@gmail.com>
2017-02-02 14:16:17 -0500

    Restore 'collectstatic' command

--- virtualboombox/settings/base.py
+++ virtualboombox/settings/base.py
@@ -13,6 +13,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'rest_framework',
     'api',
     'player',
