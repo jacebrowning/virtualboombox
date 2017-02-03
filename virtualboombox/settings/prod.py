126901b52c229899aaf0878870bfe0e5b565ace5
Jace Browning <jacebrowning@gmail.com>
2017-02-03 09:51:28 -0500

    Use whitenoise compression in production

--- virtualboombox/settings/prod.py
+++ virtualboombox/settings/prod.py
@@ -30,3 +30,5 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
